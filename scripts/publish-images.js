d1779b88e0c45d91252a19e6e2a8362e9e8b578d
reubenmiller <reuben.d.miller@gmail.com>
2025-07-25 09:58:13 +0200

    rename flow definition filename

--- scripts/publish-images.js
+++ scripts/publish-images.js
@@ -2,7 +2,7 @@ const { execSync } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 
-const flowFilename = "pipeline.toml";
+const flowFilename = "flow.toml";
 const registry = process.env.REGISTRY || "ghcr.io";
 const owner = process.env.OWNER || "thin-edge";
 
